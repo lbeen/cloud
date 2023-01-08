5ed451ac551a3ab5008862e3f42b688d4de812fd
libin79 <214652809@qq.com>
2023-01-08 20:04:47 +0800

    页面布局

--- system/src/main/java/com/mes/system/user/web/LoginController.java
+++ system/src/main/java/com/mes/system/user/web/LoginController.java
@@ -1,6 +1,9 @@
 package com.mes.system.user.web;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.util.IOUtils;
 import com.google.common.collect.Maps;
+import com.google.common.io.Files;
 import com.mes.common.system.dto.TokenUserInfo;
 import com.mes.common.system.dto.UserDTO;
 import com.mes.common.utils.AuthUtils;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -26,7 +31,7 @@ public class LoginController {
     private final UserService userService;
 
     @PostMapping("login")
-    public Result<Map<String, Object>> login(@RequestBody @Valid UserDTO userDTO) {
+    public Result<Map<String, Object>> login(@RequestBody @Valid UserDTO userDTO) throws Exception{
         SystemUser systemUser = userService.getUserByUsername(userDTO.getUsername());
         if (systemUser == null) {
             return Result.error("用户不存在");
@@ -42,6 +47,11 @@ public class LoginController {
         Map<String, Object> data = Maps.newHashMap();
         data.put(AuthUtils.TOKEN_KEY, token);
         data.put(AuthUtils.USER_KEY, userInfo);
+
+        String json = Files.toString(
+                new File("C:\\workspace\\cloud\\system\\src\\main\\java\\com\\mes\\system\\user\\web\\admin" + ".json"),
+                StandardCharsets.UTF_8);
+        data.put("menus", JSON.parseArray(json));
 
         LogUtils.logInfo(userInfo.getUsername(), "登录成功");
         return Result.success(data);
